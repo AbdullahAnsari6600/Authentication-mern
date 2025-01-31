34d7d2b1b3cb0f6662075afd3529f55957204572
AbdullahAnsari6600 <96906514+AbdullahAnsari6600@users.noreply.github.com>
2025-01-31 19:07:36 +0530

    Delete Backend directory

--- Backend/Models/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-
-mongoose.connect(process.env.mongo_url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('MongoDB connection error:', err));
